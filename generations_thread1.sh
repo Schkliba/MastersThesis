f7b1ca70d40bdbd63142ff84d75f141e39291fb5
Schkliba <frost77@post.cz>
2026-06-07 22:50:31 +0200

    generation

--- generations_thread1.sh
+++ generations_thread1.sh
@@ -1,1 +1,6 @@
-python generation_examination.py -e lunarlander -a lambda -c novelty -o "second_try" -R grid_search
+python generation_examination.py -e cartpole -a lambda -c novelty -o "third_try" -R grid_search
+python generation_examination.py -e cartpole -a diff -c novelty -o "third_try" -R grid_search
+python generation_examination.py -e cartpole -a lambda -c fitness -o "third_try" -R grid_search
+python generation_examination.py -e cartpole -a diff -c fitness -o "third_try" -R grid_search
+python generation_examination.py -e cartpole -a lambda -c add_novelty -o "third_try" -R grid_search
+python generation_examination.py -e cartpole -a diff -c add_novelty -o "third_try" -R grid_search
--- thread2.sh
+++ thread2.sh
@@ -1,8 +1,16 @@
 OUTPUT="./Data/grid_search/third_try" 
 
-python adaptive_gridsearch.py -e cartpole -a lambda -c fitness -o $OUTPUT -r bash_run -H 3
-python adaptive_gridsearch.py -e cartpole -a diff -c fitness -o $OUTPUT -r bash_run -H 3
+#python adaptive_gridsearch.py -e cartpole -a lambda -c fitness -o $OUTPUT -r bash_run -H 3
+#python adaptive_gridsearch.py -e cartpole -a diff -c fitness -o $OUTPUT -r bash_run -H 3
 #python adaptive_gridsearch.py -e lunarlander -a lambda -c novelty -o $OUTPUT -r bash_run -H 3
 #python adaptive_gridsearch.py -e lunarlander -a diff -c novelty -o $OUTPUT -r bash_run -H 3
-python adaptive_gridsearch.py -e cartpole -a lambda -c add_novelty -o $OUTPUT -r bash_run -H 3
-python adaptive_gridsearch.py -e cartpole -a diff -c add_novelty -o $OUTPUT -r bash_run -H 3
+#python adaptive_gridsearch.py -e cartpole -a lambda -c add_novelty -o $OUTPUT -r bash_run -H 3
+#python adaptive_gridsearch.py -e cartpole -a diff -c add_novelty -o $OUTPUT -r bash_run -H 3
+
+
+python adaptive_gridsearch.py -e cartpole -a lambda -c fit_archiving -o $OUTPUT -r bash_run -H 3 &
+python adaptive_gridsearch.py -e cartpole -a diff -c fit_archiving -o $OUTPUT -r bash_run -H 3 &
+wait
+python adaptive_gridsearch.py -e cartpole -a lambda -c novelty_archiving -o $OUTPUT -r bash_run -H 3 &
+python adaptive_gridsearch.py -e cartpole -a diff -c novelty_archiving -o $OUTPUT -r bash_run -H 3 &
+wait
