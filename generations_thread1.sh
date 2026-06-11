194285bb68d996567c90b606ab9498c5306fe491
František D <frost77@post.cz>
2026-06-11 21:50:57 +0200

    new me

--- generations_thread1.sh
+++ generations_thread1.sh
@@ -8,6 +8,13 @@
 # python generation_examination.py -e cartpole -a diff -c fit_archiving -o "fourth_try" -R grid_search
 # python generation_examination.py -e cartpole -a lambda -c novelty_archiving -o "fourth_try" -R grid_search &
 # python generation_examination.py -e cartpole -a diff -c novelty_archiving -o "fourth_try" -R grid_search
- python generation_examination.py -e lunarlander -a lambda -c novelty_archiving -o "fourth_try" -R grid_search &
- python generation_examination.py -e lunarlander -a diff -c novelty_archiving -o "fourth_try" -R grid_search
-wait
+# python generation_examination.py -e lunarlander -a lambda -c novelty_archiving -o "fourth_try" -R grid_search 
+# python generation_examination.py -e lunarlander -a diff -c novelty_archiving -o "fourth_try" -R grid_search
+# python generation_examination.py -e lunarlander -a lambda -c fit_archiving -o "fourth_try" -R grid_search 
+# python generation_examination.py -e lunarlander -a diff -c fit_archiving -o "fourth_try" -R grid_search
+# python generation_examination.py -e lunarlander -a lambda -c fitness -o "fourth_try" -R grid_search 
+# python generation_examination.py -e lunarlander -a diff -c fitness -o "fourth_try" -R grid_search
+# python generation_examination.py -e lunarlander -a lambda -c add_novelty -o "fourth_try" -R grid_search 
+# python generation_examination.py -e lunarlander -a diff -c add_novelty -o "fourth_try" -R grid_search
+python generation_examination.py -e lunarlander -a lambda -c novelty -o "fourth_try" -R grid_search 
+python generation_examination.py -e lunarlander -a diff -c novelty -o "fourth_try" -R grid_search
