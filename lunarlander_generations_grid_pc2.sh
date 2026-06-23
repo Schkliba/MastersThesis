e308391b3ab31223aec5a9f05cf6dfb3e9579f7a
František D <frost77@post.cz>
2026-06-23 14:04:16 +0200

    hotfix

--- lunarlander_generations_grid_pc2.sh
+++ lunarlander_generations_grid_pc2.sh
@@ -7,5 +7,4 @@ for i in "${arr[@]}"
 do
     python generation_examination.py -e lunarlander -a lambda -c $i -o $OUTPUT -R grid_search 
     python generation_examination.py -e lunarlander -a diff -c $i -o $OUTPUT  -R grid_search 
-    wait
 done
