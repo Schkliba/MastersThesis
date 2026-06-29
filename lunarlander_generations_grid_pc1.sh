38f3d0760fd5c0f5563b63df04ec0c45577de6ec
Schkliba <you@example.com>
2026-06-30 00:19:56 +0200

    gens

--- lunarlander_generations_grid_pc1.sh
+++ lunarlander_generations_grid_pc1.sh
@@ -1,7 +1,7 @@
 OUTPUT="server_try2" 
 
-#declare -a arr=("add_novelty" "sub_novelty" "novelty_limit" "novelty_archiving", "novelty")
-declare -a arr=("fitness" "fit_archiving" "elite_archiving")
+declare -a arr=("add_novelty" "sub_novelty" "novelty_limit" "novelty_archiving")
+#declare -a arr=("fitness" "fit_archiving" "elite_archiving")
 
 ## loop through above array (quotes are important if your elements may contain spaces)
 for i in "${arr[@]}"
